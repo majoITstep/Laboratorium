0fd11c870d9877168c867c21acd3f529ba1774aa
rafajdus <mrafajdus@gmail.com>
2017-11-15 15:02:09 +0100

    Add project files.

---
+++ Laboratorium/Pristroj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium
+{
+    abstract class Pristroj
+    {
+        bool stav;
+        string VyrobneCislo;
+
+        public virtual void VypisInformacieOPristroji()
+
+        { }
+
+        public abstract void VypisDetailneInformacie();
+        
+        
+    }
+}
